ee95ccaf2bab30e92ec92f7b2ec82eb4ed1db795
Anaïs <diez.anais@gmail.com>
2024-12-23 16:44:56 +0100

    ajout menu deroulant, overlay en javascript

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function () { /* permet d'afficher une slide */
     const slides = document.querySelectorAll(".carousel_inner .slide");
     const prevButton = document.querySelector(".carousel_control.prev");
     const nextButton = document.querySelector(".carousel_control.next");
@@ -32,4 +32,32 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // Initialisation : afficher la première slide
     updateSlides();
+});
+
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const menuImage = document.querySelector('nav > img[title="Menu"]');
+    const dropdownMenu = document.querySelector('.dropdown_menu');
+    const overlay = document.getElementById('overlay');
+
+    // Fonction pour afficher/masquer le menu et l'overlay
+    menuImage.addEventListener('click', function() {
+        const menu_visible = dropdownMenu.style.display === 'flex';
+
+        if (menu_visible) {
+            dropdownMenu.style.display = 'none';
+            overlay.style.display = 'none'; // Masquer l'overlay
+        } else {
+            dropdownMenu.style.display = 'flex';
+            overlay.style.display = 'block'; // Afficher l'overlay
+        }
+    });
+
+    // Fermer le menu et l'overlay quand on clique sur l'overlay
+    overlay.addEventListener('click', function() {
+        dropdownMenu.style.display = 'none';
+        overlay.style.display = 'none';
+    });
 });
