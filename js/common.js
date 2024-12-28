b2d26a7d780b2701d13f0d2f556158415cfcb0b8
anais_diez <diez.anais@gmail.com>
2024-12-28 21:54:16 +0100

    page connexion modifiée pour être page de réinitialisation mot de passe

---
+++ js/common.js
@@ -0,0 +1,49 @@
+
+/* à voir plus tard. Reproduire le header et footer dynamiquement sur chaque page*/
+
+
+/*let entete = document.getElementById("entete");
+while (entete.firstChild) {
+    entete.removeChild(entete.firstChild);
+}*/
+
+
+
+/*<nav id="entete">
+<div class="left_menu">
+    <a href="index.html" alt="accueil">Accueil</a>
+    <a href="#" alt="calendrier_memo">Calendrier & Mémo</a>
+    <a href="#" alt="guide_entretien">Guide d'entretien</a>
+    <a href="liens.html" alt="liens_ressources">Liens & Ressources</a>
+    <a href="contact.html" alt="contact"> Contact</a>
+    <a href="#" alt="profil">Profil</a>
+    <a id="deconnexion" href="#" alt="deconnexion"><img src="./Icones/off.png" alt="deconnexion">Déconnexion</a>
+</div>
+<button type="button" aria-label="toggle left menu" class="menu_toggler">
+    <span class="line l1"></span> <!--span dans un bouton, oui mais div dans un bouton, non-->
+    <span class="line l2"></span>
+    <span class="line l3"></span>
+</button>
+<img id="calendar" src="./Icones/calendar.png" title="Calendrier" alt="Logo calendrier">
+<form method="post" action="monProgramme.php">
+    <fieldset class="connexion">
+        <div>
+            <label for="name">Votre identifiant</label>
+            <input type="text" name="identifiant" placeholder="Entrez votre identifiant" minlength="2" maxlength="25" id="name" required>
+        </div>
+        <div>
+            <label for="password_1">Votre mot de passe</label>
+            <input type="password" name="password" placeholder="(8 caractères minimum)" minlength="2" maxlength="25" id="password_1" required>
+        </div>
+        <div class="checkbox_container">
+            <input type="checkbox" id="checkbox" checked />
+            <label for="checkbox">Rester connecté·e</label>
+        </div>
+        <button type="submit">Se connecter</button>
+        <p>Nouvel·le arrivant·e ?
+            Par <a href="inscription.html" alt="lien_page_inscription">ici !</a></p>
+        <a id="forgetten_password" href="connexion.html" alt="reinitialisation_mot_de_passe">Mot de passe oublié ?</a>
+    </fieldset>
+</form>
+<img id="user_button" src="./Icones/user.png" title="Connexion" alt="Logo utilisateur">
+</nav>*/
