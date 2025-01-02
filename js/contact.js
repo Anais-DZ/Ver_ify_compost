f55985066c43fe2ea0d07dd34c8a5e0f5e123642
anais_diez <diez.anais@gmail.com>
2025-01-02 18:37:10 +0100

    rajout lien après audit sur l'accessibilité

--- js/contact.js
+++ js/contact.js
@@ -24,21 +24,3 @@ textarea.addEventListener("input", function () {
         submitButton.disabled = false; // Activer le bouton si la validation réussit
     }
 });
-
-//supprimer les raccourcis du calendrier sur les autres pages autres que la page d'accueil
-document.addEventListener('DOMContentLoaded', function() {
-    // Récupération de l'élément du logo et du lien du calendrier
-    const calendarElement = document.getElementById('calendar');
-    const calendarLink = document.querySelector('a[href="#calendar-fonctionnality"]');
-
-    // Vérification si la page actuelle est différente de l'index
-    if (window.location.pathname !== '/index.html') {
-        // Supprimer l'élément du logo et le lien
-        if (calendarElement) {
-            calendarElement.remove();
-        }
-        if (calendarLink) {
-            calendarLink.remove();
-        }
-    }
-});
