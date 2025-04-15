a153a99a84382c9344e2a0dcfb434c52ada5e3e8
Your Name <you@example.com>
2025-04-15 16:14:01 +0200

    supression du menu des pages inutiles en attendant la possibilité de créer un compte

--- scripts/commun.js
+++ scripts/commun.js
@@ -1,17 +1,10 @@
 //Menu en rideau
 const burgerButton = document.querySelector(".menuToggler");
 const leftMenu = document.querySelector(".leftMenu");
-const userButton = document.querySelector("#userButton");
-const rightMenu = document.querySelector(".rightMenu");
-
 
 burgerButton.addEventListener("click", (event) => {
     event.stopPropagation(); // Evite la fermeture immédiate du menu
     toggleMenu();
-});
-userButton.addEventListener("click", (event) => {
-    event.stopPropagation(); // Evite la fermeture immédiate du menu
-    toggleUser();
 });
 
 function closeMenu() {
@@ -19,48 +12,18 @@ function closeMenu() {
     leftMenu.classList.remove("active");
 }
 
-function closeUser() {
-    rightMenu.classList.remove("active");
-    userButton.setAttribute("src", "./Icones/user.png");
-}
-
 document.addEventListener("click", (event) => {
     // Vérifie si le clic est à l'extérieur des menus et boutons
     if (!leftMenu.contains(event.target) && !burgerButton.contains(event.target)) {
         closeMenu();
     }
-
-    if (!rightMenu.contains(event.target) && !userButton.contains(event.target)) {
-        closeUser();
-    }
 });
 
 function toggleMenu() {
-    // Ferme le menu utilisateur si ouvert
-    closeUser();
-
     // Basculer l'état du menu gauche
     burgerButton.classList.toggle("active");
     leftMenu.classList.toggle("active");
 }
-
-function toggleUser() {
-
-    // Ferme le menu gauche si ouvert
-    closeMenu();
-
-    // Basculer l'état du menu utilisateur
-    rightMenu.classList.toggle("active");
-
-    if (rightMenu.classList.contains("active")) {
-        // Si la classe "active" est présente
-        userButton.setAttribute("src", "./Icones/close.png");
-    } else {
-        // Sinon
-        userButton.setAttribute("src", "./Icones/user.png");
-    }
-}
-
 
 
                     // Fonction pour les placeholders
@@ -93,49 +56,3 @@ textareas.forEach((textarea) => {
         textarea.placeholder = placeholderEnregistre;
     });
 });
-
-
-// Validation formulaire
-const identifiantMenu = document.querySelector('#name'); // Récupère du Dom
-const passwordMenu = document.querySelector('#passwordMenu');
-const identifiantMenuError = document.querySelector('#identifiantMenuError');
-const passwordMenuError = document.querySelector('#passwordMenuError');
-const submitButtonMenu = document.querySelector('#submitButtonMenu');
-
-submitButtonMenu.disabled = true;
-
-function formulaireValide() {
-    const identifiantMenuValue = identifiantMenu.value;
-    const regexIdentifiantMenu = /^(?=(.*[A-Za-z]){4,}).{5,25}$/;
-
-    const identifiantMenuValide = regexIdentifiantMenu.test(identifiantMenuValue);
-
-    const passwordMenuValue = passwordMenu.value;
-    const regexPasswordMenu = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,30}$/;
-
-    const passwordMenuValide = regexPasswordMenu.test(passwordMenuValue);
-
-
-    if (identifiantMenuValue.length > 0 && !identifiantMenuValide) {
-        identifiantMenuError.innerText = "L'identifiant doit contenir au moins 5 caractères dont 4 lettres.";
-    } else {
-        identifiantMenuError.innerText = '';
-    }
-
-    if (passwordMenuValue.length > 0 && !passwordMenuValide) {
-        passwordMenuError.innerText = 'Le mot de passe doit contenir au moins 8 caractères dont 1 chiffre, 1 lettre majuscule, 1 lettre minuscule.';
-    } else {
-        passwordMenuError.innerText = '';
-    }
-
-    // Activation du bouton
-    // if (identifiantMenuValide && passwordMenuValide) {
-    //     submitButtonMenu.disabled = false;
-    // } else {
-    //     submitButtonMenu.disabled = true; 
-    // };
-
-    submitButtonMenu.disabled = !(identifiantMenuValide && passwordMenuValide) //! a retrevailler probleme
-}
-identifiantMenu.addEventListener('keyup', formulaireValide);
-passwordMenu.addEventListener('keyup', formulaireValide);
